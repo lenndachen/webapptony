c9dbd67267e889c5383a4060713c5c52cea0f170
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-09-12 11:59:17 -0400

    made form and added it to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Diagnosis from './TherapyPage'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <h4><Diagnosis /></h4>
     </div>
   );
 }
