06ff6a0722ee3709e10243cf9df343530b4b6311
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-09-13 13:24:39 -0400

    CSS and airtable work for test diagnosis

--- src/Diagnosispage/index.js
+++ src/Diagnosispage/index.js
@@ -3,12 +3,15 @@ import React from 'react'
 const Diagnosis = () => (
     <div>
     <form>
-        <label>
-            Name:
-        <input type="text" name="name" />
+        <label className = "Name">
+            <p className="privacyclause">**Providing your full name will help us manage 
+                <br /> our files and have an organizing list of people that have taken the test.Your information is
+                <br /> kept confidential and NONE of it is sold to separate parties.** 
+            </p>
         </label>
+        <div className="together"><div className="wordname">Name</div> <input className="textname" type="text" name="name" /> </div>
         
-        <input type="submit" value="Submit" />  
+        
     </form>
     </div>
 );
