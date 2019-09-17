3191c89420bf7cd636a3e96cded151b78ff148ef
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-09-17 11:26:39 -0400

    Finished Nav. Bar

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
-import logo from '../../Assets/logo.PNG'
+import logo1 from '../../Assets/logo1.PNG'
 
 const Navigation1 = () => (
   <nav className="navbar">
-  <div>
+    <div className="logo-wrapper"><img src={logo1} alt=""></img></div>
+   
     <ul className="links-component">
-     <img src={logo} alt=""></img>
-     <div className="link-items">
-      <div className="link1">
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </div>
-      <div className="link2">
-        <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-      </div>
-      <div className="link3">
-        <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
-      </div>
-      </div>
+      <li>
+        <Link className="link-buttons" to={ROUTES.SIGN_IN}>Sign In</Link>
+      </li>
+      <li>
+        <Link className="link-buttons" to={ROUTES.SIGN_UP}>Sign Up</Link>
+      </li>
+      <li>
+        <Link className="link forget-password" to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
+      </li>
     </ul>
-  </div>
   </nav>
 );
 
