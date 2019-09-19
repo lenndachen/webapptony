10ad8c45c445a611a11ec2d698e91bb171ad5db8
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-09-19 13:30:07 -0400

    put picture into landing page with fancy button

--- src/Components/Landing/index.js
+++ src/Components/Landing/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+import females from '../../Assets/picsforlandingpage/females.jpg'
+import handgestures from "../../Assets/picsforlandingpage/handgestures.jpg"
+import talking from "../../Assets/picsforlandingpage/talking.jpg";
+import writingdown from "../../Assets/picsforlandingpage/writingdown.png";
 
 const Landing = () => (
+<div className="row">
   <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-      </li>
-      
-    </ul>
   </div>
+      <div  className="picture-component">
+        <ul>
+          <li>
+              <img className="picture photo4"  src={writingdown} alt="writing hands" />
+          </li>
+              <button> <Link className="inside-pic-btn" to={ROUTES.SIGN_IN}>Sign In</Link> </button>
+        </ul>
+      </div>
+</div>
 );
 
 export default Landing;
