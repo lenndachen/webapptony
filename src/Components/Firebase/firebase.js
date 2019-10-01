322418e556c94700cf2ed2aee34f42544c9806f4
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-10-01 11:36:18 -0400

    indentations

--- src/Components/Firebase/firebase.js
+++ src/Components/Firebase/firebase.js
@@ -1,6 +1,6 @@
-import app from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database'
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -8,16 +8,15 @@ const config = {
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
 class Firebase {
   constructor() {
-    
     app.initializeApp(config);
 
     this.auth = app.auth();
-    this.db = app.database()
+    this.db = app.database();
   }
 
   // *** Auth API ***
@@ -32,13 +31,12 @@ class Firebase {
 
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
 
-  doPasswordUpdate = password =>
-    this.auth.currentUser.updatePassword(password);
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
 
   // *** user api ***
 
   user = uid => this.db.ref(`users/${uid}`);
-  users = () => this.db.ref('user')
+  users = () => this.db.ref("user");
 
   //*** message **
 
